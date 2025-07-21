ec188c308d0de2cb144c96f8e3f28b92635d90d5
Anh Thế <anhthe@Anhs-MacBook-Pro.local>
2025-07-21 13:16:23 +0700

    deploy-production(ren der.com)

--- src/server.js
+++ src/server.js
@@ -25,6 +25,18 @@ const START_SERVER = () => {
     res.end('<h1>Hello World!</h1><hr>')
   })
 
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.PORT, () => {
+    // eslint-disable-next-line no-console
+      console.log(`Production: Hello ${env.AUTHOR}, I am running at port: ${process.env.PORT}/`)
+    })
+  } else {
+    app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+    // eslint-disable-next-line no-console
+      console.log(`Hello ${env.AUTHOR}, I am running at http://${env.LOCAL_DEV_APP_HOST}:${env.LOCAL_DEV_APP_PORT}/`)
+    })
+  }
+
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
